be5c06bc2462e1fd24b1314f834021a9fb6597d1
Usama-khanx <usamak2004@gmail.com>
2024-10-13 10:41:47 +0500

    adds alerts to show outputs

--- js/script.js
+++ js/script.js
@@ -11,19 +11,30 @@ var pet = {
 
         if(this.hunger < 0){
             this.hunger = 0;
-        }
-        else{
             alert(`${this.name} has feed Hunger is now ${this.hunger}`)
         }
+       
+       
     },
 
     play: function(){
         this.happiness += 20;
+        if(this.happiness > 100){
+            this.happiness = 100;
+            alert(`${this.name} is Happy ${this.happiness}`)
+        }
     },
 
     agePet: function(){
         this.age += 1;
+        this.happiness -= 5;
+        this.hunger += 10;
+
+        alert(`${this.name} age is ${this.age} and Happy is ${this.happiness} and Hungry is ${this.hunger}`)
     }
 }
 
-console.log("thsi")
+console.log("Hello")
+pet.feed();
+pet.play();
+pet.agePet();
