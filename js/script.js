d88f8e0d5a425bda990450fd98f6694364d053e5
Usama-khanx <usamak2004@gmail.com>
2024-10-13 10:23:52 +0500

    add a if condtion in a feed method

--- js/script.js
+++ js/script.js
@@ -2,6 +2,26 @@ var pet = {
     name: "mini",
     type: "cat",
     age: 2,
-    happiness: 23,
-    hunger: 19,
+    happiness: 77,
+    hunger: 56,
+
+
+    feed: function(){
+        this.hunger -= 20;
+
+        if(this.hunger < 0){
+            this.hunger = 0;
+        }
+        else{
+            alert(`${this.name} has feed Hunger is now ${this.hunger}`)
+        }
+    },
+
+    play: function(){
+        this.happiness += 20;
+    },
+
+    agePet: function(){
+        this.age += 1;
+    }
 }
